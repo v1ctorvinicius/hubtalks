addc4af23cf91865ddd42d7290018ff92659cede
v1ctorvinicius <victorviniciuspj@gmail.com>
2024-10-15 13:59:12 -0300

    feat: add UserStore

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
+<script setup lang="ts">
+import "./assets/style.css"
+import { useUserStore } from "~/stores/UserStore";
+
+onBeforeMount(() => {
+  const userStore = useUserStore();
+  const isAuthenticated: Ref<boolean> = ref(userStore.loadFromCookie());
+  if (!isAuthenticated.value) {
+    userStore.setIsAuthenticated(false);
+    useCookie("isAuthenticated").value = "false";
+    return;
+  }
+  userStore.setIsAuthenticated(isAuthenticated.value)
+
+})
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout />
 </template>
