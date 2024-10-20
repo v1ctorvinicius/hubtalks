f71c39397da3149bd5da5bd80ba94115e7f1a54f
v1ctorvinicius <victorviniciuspj@gmail.com>
2024-10-20 17:34:27 -0300

    config(nuxt.config): nitro websocket support and mongodb

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  // plugins: ['~/plugins/piniaPlugin.ts'],
   modules: ["@pinia/nuxt"],
   runtimeConfig: {
     public: {
       googleClientId: process.env.GOOGLE_CLIENT_ID,
       redirectUri: process.env.REDIRECT_URI,
       apiBaseUrl: process.env.API_BASE_URL,
+      mongodbPassword: process.env.MONGODB_PASSWORD,
     },
   },
-  hooks: {
-        
+  nitro: {
+    experimental: {
+      websocket: true,
+    },
   },
+  hooks: {},
 });
